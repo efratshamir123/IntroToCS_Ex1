212cb351c95363bbf7e392babd3097a3c109ab5f
Efrat <efrat.shamir200@gmail.com>
2025-11-30 12:30:00 +0200

    first commit

--- IntroToCS_Ex1/src/Main.java
+++ IntroToCS_Ex1/src/Main.java
@@ -1,11 +1,13 @@
-import Ex1.Ex1;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+  // to see how IntelliJ IDEA suggests fixing it.
+  IO.println(String.format("Hello and welcome!"));
 
-public class Main {
-    public static void main (String[]args){
-        double[] p1 = {1,2,3,4};
-        double[] p2 = {1,2,3};
-
-        boolean result = Ex1.equals(p1,p2);
-        System.out.println(result);
-}
+  for (int i = 1; i <= 5; i++) {
+    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+    IO.println("i = " + i);
+  }
 }
